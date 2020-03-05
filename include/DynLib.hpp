b1bab8c7d89a208b38e009ccc0893c5647f295a5
guillaume astorgis <guillaume.astorgis@epitech.eu>
2020-03-06 00:52:39 +0100

    Standardisation
    - Cleaned up preprocessor
    
    - Added symbol getter to windows implementation
    - Added non-function types to symbol getter
    
    - Changed method names to be more specific on waht they do
    
    - Fixed windows implementation constructor conversion from string to wstring

--- include/DynLib.hpp
+++ include/DynLib.hpp
@@ -12,9 +12,15 @@
 #include <iostream>
 
 
-	#if defined(_WIN32)	//Windows
+#if defined(_WIN32)	//Windows
 
-		#include <windows.h>
+	#include <windows.h>
+#else
+
+	#include <memory>
+	#include <dlfcn.h>
+#endif
+
 
 namespace Plug
 {
@@ -24,81 +30,22 @@ namespace Plug
 
 			DynLib(const std::string &path);
 
+		#if defined(_WIN32)
+			~DynLib();
+		#else
+			~DynLib() = default;
+		#endif
+
 			template<typename Callable = long(), typename... Args>
-			auto	call(const std::string &key, Args&&... args)
+			decltype(auto)	callSymbol(const std::string &key, Args&&... args)
 			{
 				using Functional = std::function<Callable>;
 				using FctPtr = FunctionalTarget<Callable>;
+			#if defined(_WIN32)	//Windows
 				auto	thing = GetProcAddress(_handle, key.c_str());
 				if (thing == NULL)
 					throw std::runtime_error(std::string(__func__) + " : " + (char *)GetLastError());
-				Functional	callable(reinterpret_cast<FctPtr>(thing));
-				return callable(std::forward<Args>(args)...);
-			}
-
-			~DynLib();
-
-		protected:
-		private:
-
-			template<typename T>
-			struct FunctionalTraits {};
-
-			template<typename R, typename... Args>
-			struct FunctionalTraits<std::function<R(Args...)>> {
-				using resultType = R;
-				using signature = R(*)(Args...);
-				struct argumentTypes {
-					using asTuple = std::tuple<Args...>;
-
-					template<size_t index>
-					using arg = std::tuple_element_t<index, asTuple>;
-				};
-			};
-
-			template<typename Callable>
-			using FunctionalTarget = typename FunctionalTraits<std::function<Callable>>::signature;
-
-		HINSTANCE	_handle;
-
-	};
-
-	#else //UNIX
-
-		#include <memory>
-		#include <dlfcn.h>
-
-namespace Plug
-{
-
-	///
-	///@brief Dynamic library wrapper
-	///
-	///
-	class DynLib {
-		public:
-
-			DynLib(const std::string &path);
-
-			~DynLib() = default;
-
-			///
-			///@brief Calls a function in the library
-			///
-			/// Calls the function associated with symbol, forwarding it args
-			/// using Callable as its signature
-			///
-			///@tparam Callable Function signature
-			///@tparam Args Arguments types
-			///@param symbol Symbol of the function
-			///@param args Arduments forwarded to the function
-			///@return auto Return of the called function
-			///
-			template<typename Callable, typename... Args>
-			auto	call(const std::string &symbol, Args&&... args)
-			{
-				using Functional = std::function<Callable>;
-				using FctPtr = FunctionalTarget<Callable>;
+			#else
 				auto	*prevErr = dlerror();
 				if (prevErr != nullptr)
 					std::cerr << "Warn: when loading call to \"" << symbol << "\", detected previous unhandled error: " << prevErr << '\n';
@@ -106,16 +53,43 @@ namespace Plug
 				auto	*err = dlerror();
 				if (err != nullptr)
 					throw std::runtime_error(std::string(__func__) + " : " + err);
+			#endif
 				Functional	callable(reinterpret_cast<FctPtr>(thing));
 				return callable(std::forward<Args>(args)...);
 			}
 
-			//UNIX only
-			template<typename Callable>
-			auto	get(const std::string &symbol)
+
+			template<typename T>
+			decltype(auto)	getSymbol(const std::string &symbol)
 			{
-				using Functional = std::function<Callable>;
-				using FctPtr = FunctionalTarget<Callable>;
+				using Ptr = T*;
+			#if defined(_WIN32)
+				auto	thing = GetProcAddress(_handle, key.c_str());
+				if (thing == NULL)
+					throw std::runtime_error(std::string(__func__) + " : " + (char *)GetLastError());
+			#else
+				auto	*prevErr = dlerror();
+				if (prevErr != nullptr)
+					std::cerr << "Warn: when loading pointer to \"" << symbol << "\", detected previous unhandled error: " << prevErr << '\n';
+				auto	*thing = dlsym(_handle.get(), symbol.c_str());
+				auto	*err = dlerror();
+				if (err != nullptr)
+					throw std::runtime_error(std::string(__func__) + " : " + err);
+
+			#endif
+				return *(reinterpret_cast<Ptr>(thing));
+			}
+
+			template<typename R, typename... Args>
+			decltype(auto)	getSymbol<R(Args...)>(const std::string &symbol)
+			{
+				using Functional = std::function<R(Args...)>;
+				using FctPtr = R(*)(Args...);
+			#if defined(_WIN32)
+				auto	thing = GetProcAddress(_handle, key.c_str());
+				if (thing == NULL)
+					throw std::runtime_error(std::string(__func__) + " : " + (char *)GetLastError());
+			#else
 				auto	*prevErr = dlerror();
 				if (prevErr != nullptr)
 					std::cerr << "Warn: when loading function pointer to \"" << symbol << "\", detected previous unhandled error: " << prevErr << '\n';
@@ -123,11 +97,21 @@ namespace Plug
 				auto	*err = dlerror();
 				if (err != nullptr)
 					throw std::runtime_error(std::string(__func__) + " : " + err);
+
+			#endif
+
 				return Functional(reinterpret_cast<FctPtr>(thing));
 			}
 
+
 		protected:
 		private:
+
+		#if defined(_WIN32)
+			HINSTANCE	_handle;
+		#else //UNIX
+			std::unique_ptr<void, std::function<void(void *)>>	_handle;
+		#endif /* !OS check */
 
 			template<typename T>
 			struct FunctionalTraits {};
@@ -143,17 +127,11 @@ namespace Plug
 					using arg = std::tuple_element_t<index, asTuple>;
 				};
 			};
-
 			template<typename Callable>
 			using FunctionalTarget = typename FunctionalTraits<std::function<Callable>>::signature;
 
-			std::unique_ptr<void, std::function<void(void *)>>	_handle;
-
 	};
 
-
-
-	#endif /* !OS check */
 
 } // namespace Plug
 
--- src/DynLib.cpp
+++ src/DynLib.cpp
@@ -11,8 +11,18 @@
 #if defined(_WIN32)
 //Windows
 
+static std::wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0); 
+    std::wstring r(len, L'\0');
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, &r[0], len);
+    return r;
+}
+
 Plug::DynLib::DynLib(const std::string &path):
-	_handle{LoadLibrary(path.c_str())}
+	_handle{LoadLibrary(s2ws(path).c_str())}
 {
 	if (_handle == NULL)
 		throw std::runtime_error(std::string(__func__) + " : " + (char *)GetLastError());
